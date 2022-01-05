ed4159573c2e0ce790814fd8611671c2842f9bed
Canberra <canberra.mc@gmail.com>
2022-01-05 14:46:42 +0100

    add tailwind login component

---
+++ src/scenes/login/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginCard from "../../components/cards/login-card/login-card";
+import Styles from "./login.module.css";
+
+
+export default function Login(){
+    return(
+        <>
+           <LoginCard></LoginCard>
+        </>
+    )
+}
